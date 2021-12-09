47c2024deb0f836b4baed7d6a34b33a10c1bf6c4
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-08 21:40:35 -0500

    calling openweather api to get current weather elements

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-src = "https://code.jquery.com/jquery-3.4.1.min.js"
 // var key = db51c4803ad66580db1682e7df54580f;
 var city = "Enter City Here"
 
 var cityList = [];
 
-$('.search-btn').on("click", function (search) {
-    search.preventDefault();
-    city = $(this).parent('.city-input-btn').siblings('.textValue').val().trim();
-    if (city === "") {
-        return;
-    };
-    cityList.push(city);
+// current weather info section
+var cardBodyToday = $('.cardBody')
+function getWeatherToday() {
+var getUrlCurrent = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}';
 
-    localStorage.setItem('city', JSON.stringify(cityList));
+$(cardBodyToday).empty();
 
-    console.log(search());
+$ajax({
+    url: getUrlCurrent,
+    method: 'GET',
+}).then(function (response){
+    $('.cardTodayCityName').text(response.name);
+    $('.cardTodayDate').text(date);
+
+    // icons needed 
+
+    // temp
+    var pElement = $('<p>').text('Temperature: ${response.main.temp} °F');
+    cardBodyToday.append(pElement);
+    // feels like 
+    var pTemElement = $('<p>').text('Feels Like: ${response.main.feels_like} °F');
+    cardBodyToday.append(pTemElement);
+    // humidity
+    var pHumElement = $('<p>').text('Humidity: ${response.main.humidity} %');
+    cardBodyToday.append(pHumElement);
+    // wind
+    var pWindElement = $('<p<').text('Wind Speed: ${response.wind.speed} MPH');
+    cardBodyToday.append(pWindElement);
+
+    var cityLon = response.coord.lon;
+    var cityLat = response.coord.lat;
 
 
-});
+})
+}
