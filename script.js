f8ae7150fb97a50f0682b55e06675977711bb7bc
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-08 20:56:28 -0500

    added js links

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// var currentWeather = https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily,minutely&appid=db51c4803ad66580db1682e7df54580f
+src = "https://code.jquery.com/jquery-3.4.1.min.js"
+// var key = db51c4803ad66580db1682e7df54580f;
+var city = "Enter City Here"
+
+var cityList = [];
+
+$('.search-btn').on("click", function (search) {
+    search.preventDefault();
+    city = $(this).parent('.city-input-btn').siblings('.textValue').val().trim();
+    if (city === "") {
+        return;
+    };
+    cityList.push(city);
+
+    localStorage.setItem('city', JSON.stringify(cityList));
+
+    console.log(search());
 
 
-// console.log(currentWeather);
+});
