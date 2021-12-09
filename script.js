3f5c4a6fdcbdd8331a1397a8e3aa82e44edd75fb
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-08 20:07:14 -0500

    setting up html for js code

---
+++ script.js
@@ -0,0 +1,4 @@
+// var currentWeather = https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily,minutely&appid=db51c4803ad66580db1682e7df54580f
+
+
+// console.log(currentWeather);
