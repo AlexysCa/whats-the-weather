3fa8d0cd331a66a2fd54ebcc5bb17944d573cce0
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2021-12-10 17:43:01 -0500

    set upcode to call api for open weather for 5 day forecast

--- script.js
+++ script.js
@@ -58,6 +58,41 @@ function startSite() {
             });
 
             // 5 day weather will go here
+            var cityId = response.data.id;
+            var forecastUrl = "https://sylviaprabudy.github.io/weather-dashboard/" + cityId + "&appid=" + Key;
+            axios.get(forecastUrl)
+            .then(function (response) {
+                fiveDayEl.classList.remove("d-none");
+                // displays 5 day future forecast
+                var forecastElement = document.querySelectorAll(".forecast");
+                for (i = 0; i < forecastElement.length; i++) {
+                    forecastElement[i].innerHTML = "";
+                    var forecastIndex = i * 8 + 4;
+                    var forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                    var forecastDay = forecastDate.getDate();
+                    var forecastMonth = forecastDate.getMonth() + 1;
+                    var forecastYear = forecastDate.getFullYear();
+                    var forecastDateEl = document.createElement("p");
+
+                    forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastElement[i].append(forecastDateEl);
+
+                // icons should display for current weather
+                    var forecastWeatherEl = document.createElement("img");
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                    forecastElement[i].append(forecastWeatherEl);
+
+                    var forecastTempEl = document.createElement("p");
+                    forecastTempEl.innerHTML = "Temperature: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
+                    forecastElement[i].append(forecastTempEl);
+
+                    var forecastHumidEl = document.createElement("p");
+                    forecastHumidEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                    forecastElement[i].append(forecastHumidEl);
+                }
+            })
         })
     }
 
